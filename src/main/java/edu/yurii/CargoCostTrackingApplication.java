ce50235b897a0204465af669d7c6b2d818f68775
Yurii Selebynka <selebinkayuriy@gmail.com>
2021-07-26 10:19:30 +0200

    pom configuration commit

---
+++ src/main/java/edu/yurii/CargoCostTrackingApplication.java
@@ -0,0 +1,13 @@
+package edu.yurii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoCostTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargoCostTrackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/yurii/CargoCostTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yurii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoCostTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
