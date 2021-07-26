b8dbc0d341236bb3ec63d91cbfe7c4dac66fe52b
Yurii Selebynka <selebinkayuriy@gmail.com>
2021-07-26 14:49:27 +0200

    route service commit

---
+++ src/main/java/edu/yurii/service/route/impls/RouteServiceImpl.java
@@ -0,0 +1,42 @@
+package edu.yurii.service.route.impls;
+/*
+  @author   Yurii Selebynka
+  @project   CargoCostTracking
+  @class  RouteServiceImpl
+  @version  1.0.0 
+  @since 26.07.21 - 14.48
+*/
+
+import edu.yurii.model.Route;
+import edu.yurii.service.route.interfaces.IRouteService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RouteServiceImpl implements IRouteService {
+    @Override
+    public Route create(Route route) {
+        return null;
+    }
+
+    @Override
+    public Route get(String id) {
+        return null;
+    }
+
+    @Override
+    public Route update(Route route) {
+        return null;
+    }
+
+    @Override
+    public Route delete(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Route> getAll() {
+        return null;
+    }
+}
